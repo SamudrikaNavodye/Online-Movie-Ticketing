df21b37e2cd3790c0a0e1905751a7dc860ca22d3
SamudrikaNavodye <sn96gunathilaka@gmail.com>
2024-03-22 15:12:00 +0530

    Initial commit

---
+++ src/recepit.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sn96g
+ */
+public class recepit {
+
+    public recepit(String a, String b, String e, String d, int N1, String f) {
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
